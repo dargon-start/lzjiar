47e64cf8e35f42ca49f6a45cd059c3951694a363
dargon-star <9615512+dargon-star@user.noreply.gitee.com>
2022-05-31 22:39:17 +0800

    fix(fix husky): 添加自动化验证机制

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p></p>
+        <p>
+          <div>fewaf</div>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"

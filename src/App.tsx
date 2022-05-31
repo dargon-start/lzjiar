2041b5e231cc908e7c37b843b1d1ffd4eaa60efd
dargon-star <9615512+dargon-star@user.noreply.gitee.com>
2022-05-31 20:24:28 +0800

    更正prettier

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          Edit <code>src/App.tsx</code> and save to reload.{' '}</p>
         <a
           className="App-link"
           href="https://reactjs.org"

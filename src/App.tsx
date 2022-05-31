f7969d31a937f25d6f744410c2295da788ac3fa2
dargon-star <9615512+dargon-star@user.noreply.gitee.com>
2022-05-31 20:48:12 +0800

    fix(commit-msg): add提交验证

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload. <div>fewaf</div>
-          fewafe fewaffew fewaffewaf ew
-        </p>
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"

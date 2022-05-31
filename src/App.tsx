b27c4bb35b08519eeba316b734f0aa66ea7dae70
dargon-star <9615512+dargon-star@user.noreply.gitee.com>
2022-05-31 20:17:01 +0800

    更正prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from 'logo.svg';
-import './App.css';
+import React from 'react'
+import logo from 'logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />   <p> Edit <code>src/App.tsx</code> and save to reload.</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+
+
+          
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +23,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
